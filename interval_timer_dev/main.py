c5b82495168d870b223c838223a8cb4777f5e987
Milz <yimsavan@gmail.com>
2022-06-20 11:51:34 -0400

    milz_dev_refactor
    added fonts, created widgets, renamed models and main kv files. added new classes and definitions into main.py

--- interval_timer_dev/main.py
+++ interval_timer_dev/main.py
@@ -1,1 +1,29 @@
 #! python3
+
+import time
+import models
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty, BooleanProperty
+
+
+class TimerPage(BoxLayout):
+    count = 0
+    count_enabled = BooleanProperty(False)
+    my_visual_data = StringProperty('0')
+    user_input_str = StringProperty('00')
+
+    def on_button_click(self):
+        print('button clicked')
+        if self.count_enabled is True:
+            self.count += 1
+            self.my_visual_data = str(self.count)
+
+
+class IntervalApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    interval = IntervalApp()
+    interval.run()
